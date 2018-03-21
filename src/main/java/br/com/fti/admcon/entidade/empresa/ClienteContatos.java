dd783acd0ef9f2975926e41e31a9169e5787090b
marcelinofiuza <marcelinofiuza@gmail.com>
2018-03-21 19:02:40 -0300

    Atualização

---
+++ src/main/java/br/com/fti/admcon/entidade/empresa/ClienteContatos.java
@@ -0,0 +1,87 @@
+package br.com.fti.admcon.entidade.empresa;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import br.com.fti.admcon.entidade.embeddeble.Contato;
+import br.com.fti.admcon.tenancy.ZEmpresa;
+
+/****************************************************************************
+ * Entidade Contatos Cliente Desenvolvido por :
+ * 
+ * @author Bob-Odin - 30/03/2017 /
+ ****************************************************************************/
+@Entity
+public class ClienteContatos extends ZEmpresa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long idContato;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idCliente")
+	private Cliente cliente;
+
+	@Embedded
+	private Contato contato = new Contato();
+
+	public Long getIdContato() {
+		return idContato;
+	}
+
+	public void setIdContato(Long idContato) {
+		this.idContato = idContato;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Contato getContato() {
+		return contato;
+	}
+
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idContato == null) ? 0 : idContato.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof ClienteContatos))
+			return false;
+		ClienteContatos other = (ClienteContatos) obj;
+		if (idContato == null) {
+			if (other.idContato != null)
+				return false;
+		} else if (!idContato.equals(other.idContato))
+			return false;
+		return true;
+	}
+
+}
--- src/main/resources/db/migration/V002__tabelas_principais.sql
+++ src/main/resources/db/migration/V002__tabelas_principais.sql
@@ -1,3 +1,4 @@
+/*TABELA DE CONTA CONTÁBIL*/
 CREATE TABLE conta (
   id_conta bigint(20) NOT NULL AUTO_INCREMENT,
   zempresa bigint(20) DEFAULT NULL,
@@ -11,4 +12,43 @@ CREATE TABLE conta (
   PRIMARY KEY (id_conta),
   KEY (id_conta_pai),
   CONSTRAINT FOREIGN KEY (id_conta_pai) REFERENCES conta (id_conta)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*TABELA DE CLIENTES*/
+CREATE TABLE cliente (
+  id_cliente bigint(20) NOT NULL AUTO_INCREMENT,
+  zempresa bigint(20) DEFAULT NULL,
+  cnpj varchar(18) DEFAULT NULL,
+  cpf varchar(14) DEFAULT NULL,
+  bairro varchar(40) DEFAULT NULL,
+  cep varchar(9) DEFAULT NULL,
+  cidade varchar(40) DEFAULT NULL,
+  complemento varchar(20) DEFAULT NULL,
+  logradouro varchar(50) DEFAULT NULL,
+  numero varchar(10) DEFAULT NULL,
+  tipo_logradouro varchar(15) DEFAULT NULL,
+  uf varchar(2) DEFAULT NULL,
+  fantasia varchar(40) NOT NULL,
+  ins_estadual varchar(15) DEFAULT NULL,
+  ins_municipal varchar(15) DEFAULT NULL,
+  razao_social varchar(80) NOT NULL,
+  unidade varchar(15) DEFAULT NULL,
+  id_conta bigint(20) DEFAULT NULL,
+  PRIMARY KEY (id_cliente),
+  KEY (id_conta),
+  CONSTRAINT FOREIGN KEY (id_conta) REFERENCES conta (id_conta)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE cliente_contatos (
+  id_contato bigint(20) NOT NULL AUTO_INCREMENT,
+  zempresa bigint(20) DEFAULT NULL,
+  celular varchar(15) DEFAULT NULL,
+  email varchar(100) DEFAULT NULL,
+  nome_contato varchar(50) DEFAULT NULL,
+  ramal varchar(5) DEFAULT NULL,
+  telefone varchar(15) DEFAULT NULL,
+  id_cliente bigint(20) DEFAULT NULL,
+  PRIMARY KEY (id_contato),
+  KEY (id_cliente),
+  CONSTRAINT FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
